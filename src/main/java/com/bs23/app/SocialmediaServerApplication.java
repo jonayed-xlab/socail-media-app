3b881ff9af314c7762157457ea990fe54f440646
Jonayed <jonayed@jonayed.baperi>
2024-03-09 14:20:56 +0600

    Initial project commit

---
+++ src/main/java/com/bs23/app/SocialmediaServerApplication.java
@@ -0,0 +1,13 @@
+package com.bs23.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bs23/app/SocialmediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs23.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
